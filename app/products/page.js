6601891b910d4d3b6345fda29c67a10140d03a50
Mg Sangkatoe <mgsangkatoe@Mgs-MacBook-Pro.local>
2023-08-21 12:31:54 +0630

    update

--- app/products/page.js
+++ app/products/page.js
@@ -5,7 +5,7 @@ import SpaceForNav from "@/components/SpaceForNav"
 import axios from "axios"
 
 const GetProducts = async() => {
-    const products = await axios.get(`https://ecommerce-front-ho31dxg7p-benrosang.vercel.app/api/products`)
+    const products = await axios.get(`https://ecommerce-front-dsth3ay13-benrosang.vercel.app/api/products`)
     return products.data.data
 }
 
